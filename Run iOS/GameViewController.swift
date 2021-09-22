fbef0afac0e545b98725546e0d224f85a7458f73
toyuu16 <ssta0ut0@gmail.com>
2021-09-23 01:11:52 +0900

    Initial Commit

---
+++ Run iOS/GameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GameViewController.swift
+//  Run iOS
+//
+//  Created by 田中祐輔 on 2021/09/23.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
